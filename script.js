106fba465f75a190b4b5a28928e68dce6f73642e
Fa5t <55101975+Fa5t@users.noreply.github.com>
2020-07-31 14:31:46 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-function pageSelect(x) {
-	var elems=document.getElementsByClassName('main');
-	for(var i=0; i<elems.length; i++){
-		elems[i].style.display='none';
-	}
-	elems[x].style.display='block';
+let navMenu = document.querySelectorAll(".menuicon");
+let content = document.querySelectorAll(".main");
+
+document.addEventListener('DOMContentLoaded', function(){
+    content[0].style.display='block';
+    
+    navMenu.forEach(
+        function(e) {
+            e.addEventListener("click", showPage);
+        }   
+    );
+});
+
+function showPage(){
+    document.querySelector(".clicked").classList.remove("clicked");  
+    this.classList.add("clicked");
+                    
+    let index = Array.prototype.indexOf.call(navMenu, this);
+    content.forEach(
+        function(e){
+            e.style.display = "none";
+        }
+    );
+    content[index].style.display = "block";
 }
